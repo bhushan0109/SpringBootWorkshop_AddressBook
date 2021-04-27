81a2fad8d4149986fc504b6e2fbc5a2761dca9c7
Bhushan Patil <bhupatil001@gmail.com>
2021-04-28 02:00:36 +0530

    Added: introduce controller layer and test URL for PUT call by ID

--- AddressBook_API/src/main/java/com/finovate/controller/PersonAddressController.java
+++ AddressBook_API/src/main/java/com/finovate/controller/PersonAddressController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +35,14 @@ public class PersonAddressController {
 	public ResponseEntity<ResponseDTO> getPersonAddressData() {
 		List<PersonAddressData> personData = addressService.getPersonAddressData();
 		ResponseDTO responseDTO = new ResponseDTO("Successfull got the data", personData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
+	}
+
+	@PutMapping("/update/{contId}")
+	public ResponseEntity<ResponseDTO> updatePersonAdresssData(@PathVariable("contId") int contId,
+			@RequestBody AddressDTO addressDTO) {
+		PersonAddressData addressData = addressService.updatePersonAdresssData(contId, addressDTO);
+		ResponseDTO responseDTO = new ResponseDTO("Updated address book of Id : ", addressData);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
