52eba90dc1bdcb1f327e2d31c69776fea8a3f836
Bhushan Patil <bhupatil001@gmail.com>
2021-04-27 13:19:24 +0530

    Started: project configuration setup done with added dependencies

---
+++ AddressBook_API/src/test/java/com/finovate/AddressBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finovate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
