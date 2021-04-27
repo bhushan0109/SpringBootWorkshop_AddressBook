a45cd71109f4af697d64fe572196df643e9e857d
Bhushan Patil <bhupatil001@gmail.com>
2021-04-28 01:05:32 +0530

    Added: implement the method for new create personAddress in service layer

--- AddressBook_API/src/main/java/com/finovate/service/AddressService.java
+++ AddressBook_API/src/main/java/com/finovate/service/AddressService.java
@@ -1,5 +1,17 @@
 package com.finovate.service;
 
-public class AddressService implements IAddressService{
+import org.springframework.stereotype.Service;
+
+import com.finovate.dto.AddressDTO;
+import com.finovate.model.PersonAddressData;
+
+@Service
+public class AddressService implements IAddressService {
+
+	@Override
+	public PersonAddressData createAddressData(AddressDTO addressDTO) {
+		PersonAddressData addressData = new PersonAddressData(1,addressDTO);
+		return addressData;
+	}
 
 }
--- AddressBook_API/src/main/java/com/finovate/service/IAddressService.java
+++ AddressBook_API/src/main/java/com/finovate/service/IAddressService.java
@@ -1,5 +1,10 @@
 package com.finovate.service;
 
+import com.finovate.dto.AddressDTO;
+import com.finovate.model.PersonAddressData;
+
 public interface IAddressService {
+
+	PersonAddressData createAddressData(AddressDTO addressDTO);
 
 }
