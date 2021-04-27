cef9b6c4fef7eb8c1da799eb51ccbd1d590b3b54
Bhushan Patil <bhupatil001@gmail.com>
2021-04-28 01:27:37 +0530

    Added: introduce controller layer and test URL for GET call

--- AddressBook_API/src/main/java/com/finovate/controller/PersonAddressController.java
+++ AddressBook_API/src/main/java/com/finovate/controller/PersonAddressController.java
@@ -1,8 +1,11 @@
 package com.finovate.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +23,16 @@ public class PersonAddressController {
 	private IAddressService addressService;
 
 	@PostMapping("/create")
-	public ResponseEntity<ResponseDTO>createAddressData(@RequestBody AddressDTO addressDTO) {
+	public ResponseEntity<ResponseDTO> createAddressData(@RequestBody AddressDTO addressDTO) {
 		PersonAddressData personData = addressService.createAddressData(addressDTO);
 		ResponseDTO responseDTO = new ResponseDTO("Successfully created the data ", personData);
+		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
+	}
+
+	@GetMapping(value = { "", "/", "/get" })
+	public ResponseEntity<ResponseDTO> getPersonAddressData() {
+		List<PersonAddressData> personData = addressService.getPersonAddressData();
+		ResponseDTO responseDTO = new ResponseDTO("Successfull got the data", personData);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 }
